c23dafdec77930338bafdf5fb466841d829d274c
yuriyohorodnik <47791083+yuriyohorodnik@users.noreply.github.com>
2019-04-10 12:02:54 +0300

    Log in/Sign up activities has been changed to fit all screen sizes. MapBox Activity(MainActivity) has been added.

--- app/src/main/java/com/thesupreme/wishhook/MainActivity.kt
+++ app/src/main/java/com/thesupreme/wishhook/MainActivity.kt
@@ -1,14 +1,102 @@
 package com.thesupreme.wishhook
 
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.SettingsClient
+import com.mapbox.android.core.location.LocationEngine
+import com.mapbox.android.core.location.LocationEngineListener
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.location.LocationComponent
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineListener, OnMapReadyCallback {
+
+    //1
+    val REQUEST_CHECK_SETTINGS = 1
+    var settingsClient: SettingsClient? = null
+
+    //2
+    lateinit var map: MapboxMap
+    lateinit var permissionManager: PermissionsManager
+    var originLocation: Location? = null
+
+    var locationEngine: LocationEngine? = null
+    var locationComponent: LocationComponent? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        Mapbox.getInstance(this, "pk.eyJ1IjoidGhlc3VwcmVtZTEiLCJhIjoiY2p1YXVla24yMDZtczQzbWw3bHhlOG9lcCJ9.VmAQueURxI0me6DfD3yxTg")
+        setContentView(R.layout.activity_main)
+        mapbox.onCreate(savedInstanceState)
+
+    }
+
+    @SuppressWarnings("MissingPermission")
+    override fun onStart() {
+        super.onStart()
+        mapbox.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapbox.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapbox.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapbox.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapbox.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapbox.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        if (outState != null) {
+            mapbox.onSaveInstanceState(outState)
+        }
     }
 
 
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onConnected() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+    }
 }
